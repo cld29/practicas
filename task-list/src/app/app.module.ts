d4e14e28bb169fe0e69cd27da8810f615533c663
cld29 <cldiaz29@gmail.com>
2022-10-07 19:05:19 -0300

    commit

--- task-list/src/app/app.module.ts
+++ task-list/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './comoponents/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule
