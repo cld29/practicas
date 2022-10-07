cd556a4c8f8c1929831cea3bf5a1203d15643e98
cld29 <cldiaz29@gmail.com>
2022-10-07 20:57:04 -0300

    commit

--- task-list/src/app/components/button/button.component.ts
+++ task-list/src/app/components/button/button.component.ts
@@ -8,14 +8,14 @@ import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() color:string="";
   @Input() text:string="";
-  @Output() btnClick=EventEmitter;
+  @Output() btnClick = new EventEmitter() 
 
   constructor() { }
 
   ngOnInit(): void {
   }
   onClick(){
-    // this.btnClick.emit();
+    this.btnClick.emit();
     
   }
 }
