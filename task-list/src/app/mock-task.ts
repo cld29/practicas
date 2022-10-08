dab441cc78106e753f7eb8692347184022d1dcdb
cld29 <cldiaz29@gmail.com>
2022-10-07 21:32:17 -0300

    commit

---
+++ task-list/src/app/mock-task.ts
@@ -0,0 +1,32 @@
+import{task} from "./tasks"
+
+export const TASKS: task[] = [
+    {
+        id: 1,
+        texto: "terminar el primer modulo de Angular",
+        day: 'agosto 4 a las 12:00',
+        reminder: true 
+
+    },
+    {
+        id: 2,
+        texto: "Hacer las compras para la cena",
+        day: 'agosto 4 a las 17:00',
+        reminder: true
+
+    },
+    {
+        id: 3,
+        texto: "Investigar sobre bootstrap",
+        day: 'agosto 4 a las 19:00',
+        reminder: false
+
+    },
+    {
+        id: 4,
+        texto: "leer mi libro",
+        day: 'agosto 5 a las 17:00',
+        reminder: false
+
+    }
+]
---
+++ task-list/src/app/tasks.ts
@@ -0,0 +1,6 @@
+export interface task {
+    id:number; //cuando creamos una task podria no venir
+    texto: string;
+    day: string;
+    reminder: boolean;
+}
