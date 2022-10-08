b0bb1d6db1c902a6ad6cb3e3debc4bc6ea6bfd7c
cld29 <cldiaz29@gmail.com>
2022-10-07 22:14:34 -0300

    commit

--- task-list/src/app/app.module.ts
+++ task-list/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    TasksComponent,
+    TasksItemComponent
     
   ],
   imports: [
